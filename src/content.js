a958761c4f4aec6fbf0471f52ed486313cc6ca14
Max Müller <max2005.mueller@gmx.de>
2023-11-20 18:19:01 +0100

    Adaptions for the new design of startpage

--- src/content.js
+++ src/content.js
@@ -1,24 +1,23 @@
-const baseURL = "https://duckduckgo.com/?hps=1&q=QUERY&ia=web&iaxm=maps";
+const mapsProvider = "Apple";
 
-// creates the Maps button and adds it to the toolbar
-const divElement = document.createElement('div');
-divElement.className = 'categories css-1j2749q';
+let baseURL = "";
+if (mapsProvider == "Apple") {
+  baseURL = "https://duckduckgo.com/?hps=1&q=QUERY&ia=web&iaxm=maps";
+}
+else if (mapsProvider == "Google") {
+  baseURL = "https://www.google.com/maps/place/QUERY";
+}
 
-const formElement = document.createElement('form');
-formElement.className = 'css-122cu71';
-
+// Creates the Maps button and adds it to the navbar
 const buttonElement = document.createElement('button');
-buttonElement.className = 'css-k54ldh';
 buttonElement.textContent = 'Maps';
+buttonElement.id = "maps"
+const navbar = document.querySelector("#header > div > header > div.inline-nav.css-a1y78t > div.categories.css-ze09hc");
+navbar.appendChild(buttonElement);
 
+// Opens the map on button click
 buttonElement.addEventListener('click', () => {
   const searchQuery = document.getElementById("q").value;
   const url = baseURL.replace("QUERY", searchQuery);
-  window.open(url);
+  window.location.href = url;
 });
-
-formElement.appendChild(buttonElement);
-divElement.appendChild(formElement);
-
-const targetElement = document.querySelector('.categories.css-1j2749q');
-targetElement.appendChild(divElement);
