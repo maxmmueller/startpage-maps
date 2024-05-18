21aad9e992996bead1c3eea1eaa57f64c0170a35
Max Müller <max2005.mueller@gmx.de>
2024-05-18 20:44:08 +0200

    Adjusted to new design as pointed out in #1

--- src/content.js
+++ src/content.js
@@ -12,8 +12,12 @@ else if (mapsProvider == "Google") {
 const buttonElement = document.createElement('button');
 buttonElement.textContent = 'Maps';
 buttonElement.id = "maps"
-const navbar = document.querySelector("#header > div > header > div.inline-nav.css-a1y78t > div.categories.css-ze09hc");
+buttonElement.className = "css-vh5nmu";
+
+const element =  document.querySelector('[data-testid="News"]');
+const navbar = element?.parentElement?.parentElement?.parentElement;
 navbar.appendChild(buttonElement);
+
 
 // Opens the map on button click
 buttonElement.addEventListener('click', () => {
@@ -21,3 +25,30 @@ buttonElement.addEventListener('click', () => {
   const url = baseURL.replace("QUERY", searchQuery);
   window.location.href = url;
 });
+
+
+// Create a style element
+var styleElement = document.createElement("style");
+
+// Set the CSS rules
+var cssRules = `
+  #maps {
+    display: grid;
+    grid-template-columns: repeat(5, 20%);
+    color: #9EA5BD !important;
+    background-color: #0C0D0F;
+    font-size: 13px;
+    font-weight: 500;
+    cursor: pointer;
+    padding-bottom: 13px;
+    padding-top: 0;
+    margin-top: 2px;
+    border: none;
+  }
+`;
+
+// Append the rules to the style element
+styleElement.appendChild(document.createTextNode(cssRules));
+
+// Append the style element to the head of the document
+document.head.appendChild(styleElement);
